2d6110b570f7aae6f873f29649a41dc0dd7ffec8
LexyoAi <info@lexyo.it>
2026-06-05 12:24:37 +0200

    Aggiornamento completo Lexyo

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="application-name" content="Lexyo" />
-        <meta name="theme-color" content="#a855f7" />
+        <meta name="theme-color" content="#6C47FF" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
